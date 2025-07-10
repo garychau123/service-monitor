30196352027ec7b2f1e65173e6f5934d75c903eb
Gary Chau <s4005858@student.rmit.edu.au c:; cd 'c:\Users\William\github-classroom\rmit-computing-technologies\cosc2803-apr23-workshop-3-team-244-cosc2803-apr23'; & 'C:\Program Files\Eclipse Adoptium\jdk-17.0.6.10-hotspot\bin\java.exe' '-XX:+ShowCodeDetailsInExceptionMessages' '-cp' 'C:\Users\William\AppData\Roaming\Code\User\workspaceStorage\cb4ffdbb4a40a87e648d353c4132f54f\redhat.java\jdt_ws\cosc2803-apr23-workshop-3-team-244-cosc2803-apr23_9e8e4cd8\bin' 'coreq.GitPractice>
2025-07-10 18:20:59 +1000

    springboot admin working, no applications added yet

--- src/main/java/service/monitor/serviceMonitorApplication.java
+++ src/main/java/service/monitor/serviceMonitorApplication.java
@@ -1,22 +1,22 @@
 package service.monitor;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableAdminServer
 @RestController
-public class serviceMonitorApplication {
+public class ServiceMonitorApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(serviceMonitorApplication.class, args);
+        SpringApplication.run(ServiceMonitorApplication.class, args);
     }
-   @GetMapping("/service-monitor")
+
+    @GetMapping("/service-monitor")
     public String test() {
         return "hi";
     }
 }
-
-
